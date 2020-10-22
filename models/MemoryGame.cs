f850a0dda33f53160d9c1716df18b1f519dc4433
A.H. van Hensbergen <HENA01@rocmondriaan.nl>
2020-10-22 21:49:51 +0200

    Started with the mainwindowviewmodel

---
+++ models/MemoryGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memory.models
+{
+    public class MemoryGame
+    {
+        private List<Card> _Cards;
+        public MemoryGame()
+        {
+            _Cards = new List<Card>();
+            for(int i=1; i<25; i++)
+            {
+                _Cards.Add(new Card(i));
+                _Cards.Add(new Card(i));
+            }
+        }
+        public List<Card> Cards
+        {
+            get { return _Cards; }
+        }
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = Cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card tmp = Cards[k];
+                Cards[k] = Cards[n];
+                Cards[n] = tmp;
+
+            }
+        }
+    }
+}
---
+++ viewmodels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using memory.models;
+
+namespace memory.viewmodels
+{
+    class MainWindowViewModel
+    {
+        private MemoryGame _Game;
+
+        public MainWindowViewModel()
+        {
+            _Game = new MemoryGame();
+        }
+
+        public MemoryGame Game
+        {
+            get { return _Game; }
+            set { _Game = value; }
+        }
+    }
+}
